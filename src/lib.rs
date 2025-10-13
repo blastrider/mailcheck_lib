eb21c52a5fa16468ebec764091f24e2db8709fe9
Maxime Guillemin <mguillemin@innlog.fr>
2025-10-14 00:43:23 +0200

    exports new api

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,15 @@ pub mod validator;
 pub use validator::{
     EmailError,
     NormalizedEmail, // << nouveau
+    SpecCharacters,
+    SpecClass,
+    SpecFinding,
+    SpecOptions,
+    SpecSegment,
     ValidationMode,
     ValidationReport,
     normalize_email, // << nouveau
+    normalize_email_with_spec,
     validate_email,
+    validate_email_with_spec,
 };
